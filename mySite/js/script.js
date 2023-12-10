d88182d2c700e9ed01fb198e1549550509e88795
Jormmm <146562442+Jormmm@users.noreply.github.com>
2023-12-10 14:40:19 +0200

    Add files via upload

--- mySite/js/script.js
+++ mySite/js/script.js
@@ -20,21 +20,21 @@ menuItems.forEach(function (menuItem) {
 	})
 })
 
-document.addEventListener('DOMContentLoaded', function () {
-	const links = document.querySelectorAll('a[href^="#"]')
+// document.addEventListener('DOMContentLoaded', function () {
+// 	const links = document.querySelectorAll('a[href^="#"]')
 
-	links.forEach(link => {
-		link.addEventListener('click', function (e) {
-			e.preventDefault()
+// 	links.forEach(link => {
+// 		link.addEventListener('click', function (e) {
+// 			e.preventDefault()
 
-			const targetId = this.getAttribute('href').substring(1)
-			const targetElement = document.getElementById(targetId)
+// 			const targetId = this.getAttribute('href').substring(1)
+// 			const targetElement = document.getElementById(targetId)
 
-			targetElement.scrollIntoView({
-				behavior: 'smooth',
-			})
-		})
-	})
-})
+// 			targetElement.scrollIntoView({
+// 				behavior: 'smooth',
+// 			})
+// 		})
+// 	})
+// })
 
 
